888405eaa4a65706e39fb9697328656b897e66e7
Simple Gupta <simple.gupta@happiestminds.com>
2022-11-21 12:23:41 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+import { PlaceAppointmentComponent } from './place-appointment/place-appointment.component';
+import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
+
+const routes: Routes = [
+  {
+    path:"home",
+    component:HomeComponent
+  },
+  {
+    path:"placeappointment",
+    component:PlaceAppointmentComponent
+  },
+  {
+    path:"viewappointment",
+    component:ViewAppointmentComponent
+  },
+  {
+    path:"contactus",
+    component:ContactUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
+import { PlaceAppointmentComponent } from './place-appointment/place-appointment.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
+// const appRoutes: Routes=[
+//   {path:'home' ,component:HomeComponent}
+// ]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewAppointmentComponent,
+    PlaceAppointmentComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
